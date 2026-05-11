02b30d2d3fd8295d2ee28e557838719034aad4c8
sneakerhax <sneakerhax@gmail.com>
2026-05-11 14:33:56 -0700

    Create update.sh

---
+++ Hardware/Raspberry_Pi/update.sh
@@ -0,0 +1,3 @@
+apt update                                                                                                                                   
+apt full-upgrade -y                                                                                                                          
+apt autoremove --purge -y 
